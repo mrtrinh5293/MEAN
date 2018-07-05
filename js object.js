dee37b0dc679336acd571a3228a344ad79e3a219
Duc Dang <your@email.com>
2018-07-05 15:01:16 -0500

    ninja1

--- js object.js
+++ js object.js
@@ -14,10 +14,10 @@ for(var i=0; i < students.length; i++) {
 
 let users = {
     employees: [
-        {'first_name':  'Miguel', 'last_name' : 'Jones'},
-        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
-        {'first_name' : 'Nora', 'last_name' : 'Lu'},
-        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+        {first_name:  'Miguel', last_name : 'Jones'},
+        // {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        // {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        // {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
     ],
     managers: [
        {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
