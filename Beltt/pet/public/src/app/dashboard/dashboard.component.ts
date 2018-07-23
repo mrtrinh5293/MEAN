3985cb5b903edc1092d3560efe7c18fb3fee8fb1
Duc Dang <your@email.com>
2018-07-23 15:05:51 -0500

    holo

--- Beltt/pet/public/src/app/dashboard/dashboard.component.ts
+++ Beltt/pet/public/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { PetService } from '../pet.service'
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
--- Beltt/pet/public/src/app/dashboard/search-results/search-results.component.ts
+++ Beltt/pet/public/src/app/dashboard/search-results/search-results.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NgSwitch } from '@angular/common';
+// import { NgSwitch } from '@angular/common';
 import { PetService } from '../../pet.service';
 import { Router } from '@angular/router'
 
